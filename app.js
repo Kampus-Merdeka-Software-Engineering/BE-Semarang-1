f1a4c4a3044ec1968e9a879dbc57b0560a9136c4
Dina Septy Puspayani <dinaseptypuspayani87@gmail.com>
2023-09-22 14:43:37 +0700

    configure database, get post

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 const express = require('express')
-const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+const mysql = require('mysql');
+
+const dbConnection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'ideku',
+  });
+
+dbConnection.connect((err) => {
+if (err) {
+    console.error('Database connection error:', err);
+} else {
+    console.log('Database connected successfully');
+}
+});
+
+app.get('/admin/data', (req, res) => {
+    const sqlQuery = 'SELECT * FROM datas';
+    
+    dbConnection.query(sqlQuery, (err, results) => {
+        if (err) {
+            console.error('Database query error:', err);
+            res.status(500).json({ error: 'Error fetching data from database' });
+        } else {
+            // res.json(results);
+            res.render('admin/data', {messages: results})
+        }
+    });
+});
+
+module.exports = dbConnection;
 
 // Static files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/img', express.static(__dirname + 'public/img'))
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //Views
 app.set('views', './views')
@@ -18,9 +50,9 @@ app.get('', (req, res) => {
     res.render('index', { text: 'Test'})
 })
 
-app.get('/admin/data', (req, res) => {
-    res.render('admin/data', { text: 'admin'})
-})
+// app.get('/admin/data', (req, res) => {
+//     res.render('admin/data', { text: 'admin'})
+// })
 
 app.get('/login', (req, res) => {
     res.render('admin/login', { text: 'login'})
@@ -31,11 +63,19 @@ app.post('/', (req, res) => {
   });
 
 app.post('/submit-form', (req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "application/json"
+    const query = `INSERT INTO datas(name, email, message, review)
+    VALUES (?)`;
+    const value = [req.body.name, req.body.email, req.body.message, "Unreviewed"];
+    dbConnection.query(query, [value], (err, results) => {
+        if (err) {
+            console.error('Database query error:', err);
+            res.status(500).json({ error: 'Error fetching data from database' });
+        } else {
+            res.redirect("/");
+        }
     });
-    const submitForm = JSON.stringify(req.body, null, 2);
-    res.end(submitForm);
+    // const submitForm = JSON.stringify(req.body, null, 2);
+    // res.end(submitForm);
 });
 
 //Port
---
+++ db/ideku.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 22, 2023 at 09:40 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ideku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datas`
+--
+
+CREATE TABLE `datas` (
+  `data_id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `message` text NOT NULL,
+  `review` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `datas`
+--
+
+INSERT INTO `datas` (`data_id`, `name`, `email`, `message`, `review`) VALUES
+(1, 'Dina Septy Puspayani', 'dinaseptypuspayani87@gmail.com', 'Test message', 'Unreviewed'),
+(2, 'Dina Septy Puspayani', 'dinaseptypuspayani87@gmail.com', 'asdasd', 'unreview'),
+(3, 'Dina Septy Puspayani', 'dinaseptypuspayani87@gmail.com', 'asdasd', 'unreview'),
+(4, 'Dina Septy Puspayani', 'dinaseptypuspayani87@gmail.com', 'asdasd', 'unreview'),
+(5, 'Dina Septy Puspayani', 'dinaseptypuspayani87@gmail.com', 'test1234', 'unreview');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `datas`
+--
+ALTER TABLE `datas`
+  ADD PRIMARY KEY (`data_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `datas`
+--
+ALTER TABLE `datas`
+  MODIFY `data_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
